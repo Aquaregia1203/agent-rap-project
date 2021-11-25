18f1e54e94bddea4fbb1123ce0e63977570a63c3
KimMinJae <alswo9588@gmail.com>
2021-11-26 01:26:27 +0900

    merge Controller (테스트 버전)

--- src/main/java/kr/co/rap/agent/process/ProcessUtil.java
+++ src/main/java/kr/co/rap/agent/process/ProcessUtil.java
@@ -1,13 +1,17 @@
 package kr.co.rap.agent.process;
 
-import com.pi4j.io.gpio.*;
+import okhttp3.*;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
+import java.io.IOException;
 import java.util.Map;
 
 @Component
 public class ProcessUtil implements Runnable {
+    //final GpioController controller = GpioFactory.getInstance();
+    //final Pin OUTPUT_GPIO_01 = RaspiPin.GPIO_01;
+
+
     public void run() {
         executeManufacture(ProcessController.inputInfo);
     }
@@ -15,34 +19,35 @@ public class ProcessUtil implements Runnable {
 
     public void executeManufacture(InputInfo inputInfo) {
         try {
-            while (!viewContactSwitch()) {
-                controlLED(true);
-                Thread.sleep(500);
-                controlLED(false);
-            }
+ //           while (!viewContactSwitch()) {  // 접촉 스위치 상태가 false인 경우는 0.5초 간격으로 LED 제어
+//                onLED();
+//                Thread.sleep(500);
+//                offLED();
+//                Thread.sleep(500);
+//            }
+//
+//            controlPump(inputInfo);  // 접촉 스위치 상태가 true인 경우 원재료 통 별 펌프를 제어함.
+//
+//            Thread.sleep(10000);  //10초 대기.
+//
+//            int productWeight = measureProductWeight();  //10초가 흐른 후 무게를 측정한 값을 저장한다.
+//
+//            onLED();   //  무게 측정 후 LED를 점등.
+//
+//            while (viewContactSwitch()) {
+//                continue;
+//            }
+//
+//            Map<String, Integer> product = new HashMap<String, Integer>();
+//            product.put("productWeight", productWeight);
+//
+//            sendProductInfo(product);
 
-            controlPump(inputInfo);
-
-            Thread.sleep(10000);
-
-            int productWeight = measureProductWeight();
-
-            controlLED(true);
-
-            while (viewContactSwitch()) {
-                continue;
-            }
-
-            Map<String, Integer> product = new HashMap<String, Integer>();
-            product.put("productWeight", productWeight);
-
-            sendProductInfo(product);
-
-            controlLED(false);
+            flashLED();
 
             inputInfo = null;
         } catch (Exception e) {
-
+            e.printStackTrace();
         }
     }
 
@@ -61,14 +66,11 @@ public class ProcessUtil implements Runnable {
 
     }
 
-    public void controlLED(boolean LEDStatus) {
-        final Pin OUTPUT_GPIO_01 = RaspiPin.GPIO_01;
-        final GpioController controller = GpioFactory.getInstance();
-
-        try {
+    public void flashLED() {
+       /* try {
             final GpioPinDigitalOutput pin =
-                    controller.provisionDigitalOutputPin(OUTPUT_GPIO_01,
-                            "LED", PinState.LOW);
+                controller.provisionDigitalOutputPin(OUTPUT_GPIO_01,
+                        "LED", PinState.LOW);
 
             for (int i = 0; i < 10; i ++) {
                 pin.high();
@@ -81,7 +83,15 @@ public class ProcessUtil implements Runnable {
             e.printStackTrace();
         } finally {
             controller.shutdown();
-        }
+        }*/
+    }
+
+    public void onLED() {
+
+    }
+
+    public void offLED() {
+
     }
 
     public int measureProductWeight() {
@@ -89,6 +99,33 @@ public class ProcessUtil implements Runnable {
     }
 
     public void sendProductInfo(Map<String, Integer> productInfo) {
+        //172.16.30.115 상대 서버 측 IP
+        String url = "http://172.16.30.115/product";
+        StringBuffer message = new StringBuffer();
+        message.append("{");
+        message.append("  \"id\":수헌맨");
+        message.append("  \"password\":미안해요ㅜ,.ㅜ");
+        message.append("}");
 
+        try {
+            post(url, message.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public String post(String url, String json) throws IOException {
+        RequestBody body = RequestBody.create(MediaType.get("application/json; charset=utf-8"), json);
+        Request request = new Request.Builder()
+                                            .url(url)
+                                            .post(body)
+                                            .build();
+        try {
+            OkHttpClient client = new OkHttpClient();
+            Response response = client.newCall(request).execute();
+            return response.body().string();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
